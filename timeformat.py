2acef2b5161497a59a9512522aaee94ac2c376d8
Mateus Souza <61070878+MatteusSouza@users.noreply.github.com>
2022-04-25 12:15:47 -0300

    Fixed timeformat.py

--- timeformat.py
+++ timeformat.py
@@ -1,7 +1,9 @@
 from datetime import datetime
+from calendar import monthrange
 
 def setTime(lastHour = 0, lastMinute = 0):
     now = datetime.utcnow()
+    month_range = monthrange(now.year, now.month)[1]
     ss = now.second
     mm = now.minute - lastMinute
     HH = now.hour - lastHour
@@ -16,10 +18,10 @@ def setTime(lastHour = 0, lastMinute = 0):
         mm = 60-(-mm)
         HH -= 1
     if HH < 0:
-        HH = 60-(-mm)
+        HH = 24-(-HH)
         DD -=1
     if DD < 0:
-        DD = 24-(-DD)
+        DD = month_range-(-DD)
         MM -= 1
     if MM <0:
         MM = 12-(-MM)
